5a14cf1c83b8c01a53213dd2842666feac961476
Filipe Sousa <filpess@hotmail.com>
2019-09-16 15:00:27 -0300

    Updates

---
+++ precache-manifest.f1ce0f65868bd18a3eb0d43e3181fa24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8645d62d209dc59535114ebbb1fe329c",
+    "url": "/portfolio-web/index.html"
+  },
+  {
+    "revision": "594d44d99b757a065afb",
+    "url": "/portfolio-web/static/css/main.cd9beceb.chunk.css"
+  },
+  {
+    "revision": "4ee513745bdb06f6bcea",
+    "url": "/portfolio-web/static/js/2.fab1d972.chunk.js"
+  },
+  {
+    "revision": "594d44d99b757a065afb",
+    "url": "/portfolio-web/static/js/main.ed78ebfd.chunk.js"
+  },
+  {
+    "revision": "a894a7665d875e6ee0c7",
+    "url": "/portfolio-web/static/js/runtime~main.c3327e7a.js"
+  }
+]);
